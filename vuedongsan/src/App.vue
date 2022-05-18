dcd471e38319bcb0725494069af8b06e8efd005e
GitMume <imrebecca@naver.com>
2022-05-18 23:58:50 +0900

    22-05-18

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -5,6 +5,7 @@
   <div>
     <h4>{{ products[0] }}</h4>
     <p>50 만원</p>
+    <button @click="신고수++">허위매물신고</button> <span>신고수 : {{신고수}}</span>
   </div>
   <div>
     <h4>{{ products[1] }}</h4>
@@ -17,13 +18,14 @@
 </template>
 
 <script>
-//document.getElementById().innerHTML = ??  기존의 자바스크립트 문법
+//document.getElementById().innerHTML = ??  기존의 자바스크립트 문법.
 
 export default {
   name: "App",
   data() {
     //데이터 보관 통, 데이터는 object형으로 저장
     return {
+      신고수 : 0,
       메뉴들: ["Home", "Shop", "About"],
       products: ["역삼동원룸", "천호동원룸", "마포구원룸"],
     };
