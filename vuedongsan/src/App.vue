ed93f0b8bbbbce8c922cfc031863ebb8840343a6
GitMume <imrebecca@naver.com>
2022-05-20 02:56:44 +0900

    22-05-20 신고버튼 만들기

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -2,19 +2,30 @@
   <div class="menu">
     <a v-for="작명 in 메뉴들" :key="작명">{{ 작명 }}</a>
   </div>
+  
+  <div v-for="(a,i) in 신고수" :key="i">
+    <h4>{{ products[i] }}</h4>
+    <p>50 만원</p>
+    <button @click="신고수[i]++">허위매물신고</button> <span>신고수 : {{신고수[i]}}</span>
+  </div>
+  
+  <!--
   <div>
     <h4>{{ products[0] }}</h4>
     <p>50 만원</p>
-    <button @click="신고수+=1">허위매물신고</button> <span>신고수 : {{신고수}}</span>
+    <button @click="신고수[0]++">허위매물신고</button> <span>신고수 : {{신고수[0]}}</span>
   </div>
   <div>
     <h4>{{ products[1] }}</h4>
     <p>가격은 아무거나</p>
+    <button @click="신고수[1]++">허위매물신고</button> <span>신고수 : {{신고수[1]}}</span>
   </div>
   <div>
     <h4>{{ products[2] }}</h4>
     <p>가격은 아무거나</p>
+    <button @click="신고수[2]++">허위매물신고</button> <span>신고수 : {{신고수[2]}}</span>
   </div>
+-->
 </template>
 
 <script>
@@ -25,10 +36,16 @@ export default {
   data() {
     //데이터 보관 통, 데이터는 object형으로 저장
     return {
-      신고수 : 0,
+      신고수 : [0,0,0],
       메뉴들: ["Home", "Shop", "About"],
       products: ["역삼동원룸", "천호동원룸", "마포구원룸"],
     };
+  },
+  methods :{
+    increase(){
+      this.신고수+=1;
+    }
+
   },
   components: {},
 };
