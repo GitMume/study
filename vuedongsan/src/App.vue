2881b2e3d098ecc6db208f3e453f5b7496d96d25
GitMume <imrebecca@naver.com>
2022-05-21 00:45:57 +0900

    22-05-21 모달창 만들기

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -1,32 +1,45 @@
 <template>
+
+  <div class="black-bg" v-if="모달창열렸니 == true">
+    <div class="white-bg"> 
+      <h4>상세페이지</h4>
+      <p>상세페이지 내용</p> <button @click="모달창열렸니 = false">닫기</button>
+     </div>
+  </div>
+
+
   <div class="menu">
     <a v-for="작명 in 메뉴들" :key="작명">{{ 작명 }}</a>
   </div>
+
   
+  <!--
   <div v-for="(a,i) in 신고수" :key="i">
-  <img src="./assets/room0.jpg"
+  <img src="./assets/room0.jpg">
     <h4>{{ products[i] }}</h4>
     <p>50 만원</p>
     <button @click="신고수[i]++">허위매물신고</button> <span>신고수 : {{신고수[i]}}</span>
   </div>
+  -->
   
-  <!--
   <div>
-    <h4>{{ products[0] }}</h4>
+      <img src="./assets/room0.jpg" class="room-img">
+    <h4 @click="모달창열렸니 = true">{{ products[0] }}</h4>
     <p>50 만원</p>
     <button @click="신고수[0]++">허위매물신고</button> <span>신고수 : {{신고수[0]}}</span>
   </div>
   <div>
+      <img src="./assets/room1.jpg" class="room-img">
     <h4>{{ products[1] }}</h4>
     <p>가격은 아무거나</p>
     <button @click="신고수[1]++">허위매물신고</button> <span>신고수 : {{신고수[1]}}</span>
   </div>
   <div>
+      <img src="./assets/room2.jpg" class="room-img">
     <h4>{{ products[2] }}</h4>
     <p>가격은 아무거나</p>
     <button @click="신고수[2]++">허위매물신고</button> <span>신고수 : {{신고수[2]}}</span>
   </div>
--->
 </template>
 
 <script>
@@ -37,6 +50,7 @@ export default {
   data() {
     //데이터 보관 통, 데이터는 object형으로 저장
     return {
+      모달창열렸니 : false,
       신고수 : [0,0,0],
       메뉴들: ["Home", "Shop", "About"],
       products: ["역삼동원룸", "천호동원룸", "마포구원룸"],
@@ -53,6 +67,26 @@ export default {
 </script>
 
 <style>
+body{
+  margin:0
+}
+div{
+  box-sizing:border-box;
+}
+.black-bg{
+  width:100%; height:100%;
+  background:rgba(0,0,0,0.5);
+  position:fixed; padding:20px;
+}
+.white-bg{
+  width:100%; background: white;
+  border-radius: 8px;
+  padding:20px;
+}
+.room-img{
+  width:100%;
+  margin-top:40px;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
